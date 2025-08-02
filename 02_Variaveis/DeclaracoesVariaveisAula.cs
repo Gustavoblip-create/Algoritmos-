de66597af6e3687a34a123056eeb7e5cd620c30c
LUIS GUSTAVO DE SOUSA SOEIRO <luis.gssoeiro@senacsp.edu.br>
2025-08-01 22:05:19 -0300

    Tarefa declaracao de variaveis

---
+++ 02_Variaveis/DeclaracoesVariaveisAula.cs
@@ -0,0 +1,10 @@
+using System;
+ class DeclaracaoVariaveisDecimal
+	{
+		static void Main(string[]arges)
+		{
+			//decimal myMoneyError = 300.0;
+			decimal myMoneyCorrect = 300.0m;
+			console.WriteLine(myMoneyCorrect))
+			}
+	}	
