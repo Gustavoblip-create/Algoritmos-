c6ce083bc1e5ba2b3ead97d500db0f859caa422a
LUIS GUSTAVO DE SOUSA SOEIRO <luis.gssoeiro@senacsp.edu.br>
2025-08-01 21:47:22 -0300

    declaração de variaveis

---
+++ 02_Variaveis/DeclaracoesVariaveis.cs
@@ -0,0 +1,27 @@
+ using System;
+
+ class DeclaracaoVariaveis
+{
+	static void Main()
+	{
+			/*Declaração de variavel
+			*/
+		int a ; 
+		//inicializacao da variavel
+		a = 22;
+		Console.WriteLine(a);
+		
+		//Reatribuição de valor da variavel
+		
+		a=56;
+		Console.WriteLine(a);
+		
+		//Outras formas de declarar variaveis
+		
+		//Declaracao e inicializacao a variavel
+		int b = 925;
+		Console.WriteLine(b);
+		int y = 10, x = 20;
+		int z = y + x;
+	}
+}
