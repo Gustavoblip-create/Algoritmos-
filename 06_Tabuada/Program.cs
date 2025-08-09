98d1cea7d9c774e986b38357510423df797e3cd1
luis.gssoeiro <luisgustavomarilia52@gmail.com>
2025-08-08 22:22:47 -0300

    tabuada

---
+++ 06_Tabuada/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _06_Tabuada
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número:");
+            int numero = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"\nTabuada do número {numero}:\n");
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{numero} x {i} = {numero * i}");
+            }
+
+            Console.WriteLine("\nPressione qualquer tecla para sair...");
+            Console.ReadKey();
+        }
+    }
+}
